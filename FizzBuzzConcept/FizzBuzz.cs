f75d5f1a1cf04f89526fce8be20a271842f09b91
Bhava <errorcoder143@gmail.com>
2022-09-24 10:42:55 +0530

    Fizz Buzz Concept Committing

---
+++ FizzBuzzConcept/FizzBuzz.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzConcept
+{
+    public class FizzBuzz
+    {
+        public static void Logic()
+        {
+
+            Console.WriteLine("Enter a Any Number");
+            int Num = int.Parse(Console.ReadLine());
+
+            if (Num > 0)
+            {
+                for (int i = 1; i <= Num; i++)
+                {
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        Console.Write("FizzBuzz");
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        Console.Write("Buzz");
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        Console.Write("Fizz");
+                    }
+                    else
+                    {
+                        Console.Write(i);
+                    }
+                    Console.Write(" ");
+                }
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Number...!");
+            }
+        }
+    }
+}
